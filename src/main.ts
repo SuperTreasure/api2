501d4bb1e85023d23581dc626734c80f2abf9e4e
SuperTreasure <1933990359@qq.com>
2024-09-26 23:47:18 +0800

    chore: 修改端口号

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({ type: VersioningType.URI });
-  await app.listen(3000);
+  await app.listen(5253, '0.0.0.0', () => {
+    console.log(`Server start on http://0.0.0.0:5253`);
+  });
 }
 bootstrap();
